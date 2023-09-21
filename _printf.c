1d08c95993ede67fde0eb117694f59de65ebab39
Anthony-cloud-1 <tonysack13@gmail.com>
2023-09-20 17:16:48 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -49,21 +49,6 @@ int _printf(const char *format, ...)
 						break;
 					}
 
-				case 'i':
-					{
-						int num = va_arg(args, int);
-
-						printed_chars += print_integer(num);
-						break;
-					}
-
-				case 'd':
-					{
-						int num = va_arg(args, int);
-
-						printed_chars += print_integer(num);
-						break;
-					}
 
 				case '%':
 					write(1, "%", 1);
@@ -86,49 +71,5 @@ int _printf(const char *format, ...)
 	}
 
 	va_end(args);
-	return (printed_chars);
-}
-/**
- * print_integer - Print an integer
- * @n: The integer to print
- * Return: Number of characters printed
- */
-int print_integer(int n)
-{
-	char buffer[12];
-	int printed_chars = 0;
-
-	if (n == 0)
-	{
-		write(1, "0", 1);
-		return (1);
-	}
-
-	int i = 0;
-	int is_negative = 0;
-
-	if (n < 0)
-	{
-		is_negative = 1;
-		n = -n;
-	}
-
-	while (n > 0)
-	{
-		buffer[i++] = (n % 10) + '0';
-		n /= 10;
-	}
-
-	if (is_negative)
-	{
-		buffer[i++] = '-';
-	}
-
-	for (int j = i - 1; j >= 0; j--)
-	{
-		write(1, &buffer[j], 1);
-		printed_chars++;
-	}
-
 	return (printed_chars);
 }
