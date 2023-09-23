1084daef165245cfb1020ad6308ef08b854b29f6
Fomonyuy-Claire <fomonyuymarie2000@gmail.com>
2023-09-22 17:01:44 -0700

    printf

--- main.h
+++ main.h
@@ -9,7 +9,7 @@ int parse_format(const char *format, va_list args);
 int print_string(const char *str);
 int print_hex(int value);
 int print_integer(int num);
-int print_unsigned int(unsigned int num);
+int print_unsigned(unsigned int num);
 int print_octal(unsigned int num);
 int print_unsigned_recursive(unsigned int num);
 int print_octal_recursive(unsigned int num);
